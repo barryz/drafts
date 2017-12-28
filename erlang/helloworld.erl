6df792cf600d189d52608cd0c56c2d1a19f40fc4
barryz <barryzxb@gmail.com>
2017-12-28 22:27:12 +0800

    update helloworld.erl

--- erlang/helloworld.erl
+++ erlang/helloworld.erl
@@ -3,7 +3,7 @@
 -module(greet).
 
 %% compile attribute specified, ignore export attr
--export([hello/0, add/2, greet_and_add_two/1, judge/1, heh_fine/0, oh_god/1, help_me/1]).
+-export([hello/0, add/2, greet_and_add_two/1, judge/1, heh_fine/0, oh_god/1, help_me/1, insert/2, weather/1]).
 
 %% import
 %% -import(io, [format/2]).
@@ -56,3 +56,35 @@ help_me(Animal) ->
               true -> "fgdadfgna"
            end,
     {Animal, "says " ++ Talk ++ "!"}.
+
+
+%% case of
+
+%% c(greet).
+%% greet:insert(3, []).
+%% greet:insert(3, [1, 2, 4]).
+%% greet:insert(3, [1, 2, 3]).
+insert(X, []) -> 
+    [X];
+insert(X, Set) ->
+    case lists:member(X, Set) of 
+        true -> Set;
+        false -> [X|Set]
+    end.
+
+
+%% c(greet).
+%% greet:weather({shanghai, 20}).
+%% greet:weather({shanghai, 26}).
+%% greet:weather({beijing, 26}).
+weather(Temp) ->
+    case Temp of
+        {shanghai, N} when N > 10, N < 25 ->
+            "Good Temperature for Shanghai!";
+        {shanghai, N} when N >= 25 ->
+            "Bad Temperature for Shanghai!";
+        {beijing, N} when N > 5, N < 20 ->
+            "Good Temperature for Beijing!";
+        _ -> 
+            "Invalid record!"
+    end.
